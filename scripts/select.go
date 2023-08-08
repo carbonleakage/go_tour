c386eb84c9c6a67691788f52248731594e3c0cb2
Sarav Shanmugam <s.shanmugam@accure.net>
2023-08-08 21:30:43 +0200

    finished binary trees exercise

--- scripts/select.go
+++ scripts/select.go
@@ -14,3 +14,17 @@ func fibonacci(c, quit chan int) {
 		}
 	}
 }
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
+
+}
